5d996adc2590ac8e4f91dba141aa9f9578744d86
pudgefury <bahubali0928@mail.ru>
2022-02-06 15:47:08 -0500

    update

---
+++ src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="app" id="app">
+    <div v-if="!account">
+      <button class="primary" @click="connect">
+        Connect Wallet To Get Started
+      </button>
+    </div>
+    <div v-else>
+      <div>address:{{ account }}</div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App",
+  data() {
+    return {};
+  },
+  methods: {
+    async connect() {
+      await this.$store.dispatch("connect", 1);
+    },
+  },
+  async mounted() {
+    await this.$store.dispatch("connect");
+  },
+  computed: {
+    account() {
+      return this.$store.getters.account;
+    },
+  },
+  components: {},
+};
+</script>
+
+<style>
+</style>
---
+++ src/store/index.js
@@ -0,0 +1,77 @@
+import Vue from "vue";
+import Vuex from "vuex";
+import { ethers } from "ethers";
+
+Vue.use(Vuex);
+
+export default new Vuex.Store({
+  state: {
+    account: null,
+  },
+  getters: {
+    account: (state) => state.account,
+  },
+  mutations: {
+    setAccount(state, account) {
+      state.account = account;
+    },
+  },
+  actions: {
+    async connect({ commit, dispatch }, connect) {
+      try {
+        const { ethereum } = window;
+        if (!ethereum) {
+          commit("setError", "Metamask not installed!");
+          return;
+        }
+        if (!(await dispatch("checkIfConnected")) && connect) {
+          await dispatch("requestAccess");
+        }
+        await dispatch("checkNetwork");
+      } catch (error) {
+        console.log(error);
+        commit("setError", "Account request refused.");
+      }
+    },
+    async checkNetwork({ commit, dispatch }) {
+      let chainId = await ethereum.request({ method: "eth_chainId" });
+      const rinkebyChainId = "0x4";
+      if (chainId !== rinkebyChainId) {
+        if (!(await dispatch("switchNetwork"))) {
+          commit(
+            "setError",
+            "You are not connected to the Rinkeby Test Network!"
+          );
+        }
+      }
+    },
+    async switchNetwork() {
+      try {
+        await ethereum.request({
+          method: "wallet_switchEthereumChain",
+          params: [{ chainId: "0x4" }],
+        });
+        return 1;
+      } catch (switchError) {
+        return 0;
+      }
+    },
+    async checkIfConnected({ commit }) {
+      const { ethereum } = window;
+      const accounts = await ethereum.request({ method: "eth_accounts" });
+      if (accounts.length !== 0) {
+        commit("setAccount", accounts[0]);
+        return 1;
+      } else {
+        return 0;
+      }
+    },
+    async requestAccess({ commit }) {
+      const { ethereum } = window;
+      const accounts = await ethereum.request({
+        method: "eth_requestAccounts",
+      });
+      commit("setAccount", accounts[0]);
+    },
+  },
+});
